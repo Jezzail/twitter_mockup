d8bc8adc6d6e9189ce6b17810a83824f44a2c7c0
Jezzail <pat43607@gmail.com>
2022-07-29 00:22:57 +0900

    clean

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ module.exports = {
     domains: ["rb.gy"],
   },
   env: {
-    GOOGLE_ID:
-      "556746757627-tpt0req8nc3fb3d5c7dd5n1bdijrloje.apps.googleusercontent.com",
-    GOOGLE_SECRET: "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",
-    NEXTAUTH_URL: "http://localhost:3000/",
-    SECRET: "46pfOdkvbiJ3CpiH2lJLpym5B0qJtiDUwZZB0yHKh1A=",
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    SECRET: process.env.SECRET,
   },
 };
