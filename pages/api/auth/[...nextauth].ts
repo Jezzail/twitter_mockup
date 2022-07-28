94240e4bf6f6862c36a456831bc8ceb5a3576830
Jezzail <pat43607@gmail.com>
2022-07-29 01:56:53 +0900

    clean

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,6 +22,9 @@ export const authOptions: NextAuthOptions = {
   theme: {
     colorScheme: "dark",
   },
+  pages: {
+    signIn: "/",
+  },
   callbacks: {
     async session({ session, token }) {
       session.user.tag = session.user
